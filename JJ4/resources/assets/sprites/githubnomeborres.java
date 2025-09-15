20adfd86209b8fe522906d229ffca4e062b61f64
Jubiito1 <bollingerjulianguido@gmail.com>
2025-09-15 17:04:32 -0300

    Delete JJ4 directory

--- JJ4/resources/assets/sprites/githubnomeborres.java
+++
@@ -1,5 +0,0 @@
-package assets.sprites;
-
-public class githubnomeborres {
-
-}
--- JJ4/src/com/mijuego/entities/enemies/Enemies.java
+++
@@ -1,7 +0,0 @@
-package com.mijuego.entities.enemies;
-
-import com.mijuego.entities.Entities;
-
-public class Enemies extends Entities{
-
-}
